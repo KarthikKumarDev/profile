6a0b6c135be2dfd8e47063357c58513f01fb5081
Karthik Kumar <Karthik.Kumar@quantium.co.in>
2019-07-18 14:53:18 +0530

    Profile and Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import "./Header.scss";
+class Header extends Component {
+
+  render() { 
+    return ( <div className="profile-header"> Karthik Kumar
+    </div> );
+  }
+}
+ 
+export default Header;
---
+++ src/pages/Profile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+
+class Profile extends Component {
+  state = {  }
+  render() { 
+    return ( <Header/>);
+  }
+}
+ 
+export default Profile;
